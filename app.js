785abc1ca8968bc9ac47a4cc7fb2308f2d7f9f20
Bruce Johnson <brucejohnson@Bruces-MacBook-Pro-2.local>
2019-02-19 20:32:08 -0500

    Complete Bare Minimum Requirements

--- app.js
+++ app.js
@@ -5,11 +5,26 @@
 class GroceryItem extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      clicked: false,
+    };
+  }
+  onHover() {
+    this.setState({
+      clicked: !this.state.clicked,
+    });
+    console.log(this.state.clicked);
   }
 
   render() {
     //console.log(this.props.groceryItem);
-    return <li>{this.props.groceryItem}</li>;
+
+    var style = {
+      'font-weight': this.state.clicked ? 'bold' : 'normal'
+    };
+
+    return <li style ={style} onMouseOver={this.onHover.bind(this)} >{this.props.groceryItem}</li>;
   }
 }
 
@@ -17,7 +32,9 @@ var App = (props) => (
   <ul>
     {props.items.map((item) => {
       console.log(item);
-      return <GroceryItem groceryItem={item} />;
+      return (
+        <GroceryItem groceryItem={item} />
+      );
     })}
   </ul>
 );
